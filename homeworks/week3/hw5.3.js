6ed74c90bfea2dc188daa727b6887a15ca1256ce
lagom0327 <jz00z0019@gapp.nthu.edu.com>
2019-05-03 01:45:05 +0800

    Week3 hw5 array to string ...

--- homeworks/week3/hw5.3.js
+++ homeworks/week3/hw5.3.js
@@ -1,16 +1,15 @@
 function add(a, b) {
   const maxLength = a.length > b.length ? a.length : b.length;
-  const arrAdd = [];
+  let strAdd = '';
   let overflow = 0; // 溢位使用
   for (let i = 0; i < maxLength; i++) {
     let ad = parseInt((a[a.length - 1 - i] ? a[a.length - 1 - i] : '0'), 10) + parseInt((b[b.length - 1 - i] ? b[b.length - 1 - i] : '0'), 10) + overflow;
     // from yakim-shu week3 hw5
     overflow = ad > 9 ? 1 : 0;
     ad %= 10;
-    arrAdd.unshift(ad);
+    strAdd = ad + strAdd;// arrAdd.unshift(ad);
   }
-  if (overflow === 1) arrAdd.unshift('1');
-  return arrAdd.join('');
+  return overflow ? `1${strAdd}` : strAdd;
 }
 
 module.exports = add;
