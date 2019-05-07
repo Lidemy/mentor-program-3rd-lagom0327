211d88d924d9bbdd01eecf708b5433a13d053ed8
lagom0327 <jz00z0019@gapp.nthu.edu.com>
2019-05-07 23:39:48 +0800

    week4 updat challenge

--- homeworks/week4/getTopStreamsOfLOL.js
+++ homeworks/week4/getTopStreamsOfLOL.js
@@ -1,28 +1,43 @@
 const request = require('request');
 // from https://discuss.dev.twitch.tv/t/node-js-using-api-example/16904
 
-let after = '&after=';
+let after = '';
+const requestTimes = 2;
+let counterTimes = 0;
 
 const options = {
   method: 'GET',
-  url: `https://api.twitch.tv/helix/streams?game_id=21779&first=100${after}`,
+  url: 'https://api.twitch.tv/helix/streams?game_id=21779&first=100&after=',
   headers:
   {
     'Client-ID': 'rjwb8zewf0hx6k2wdskymmxmzy7tpa',
   },
 };
 
-const topStreamsOfLOL = () => {
-  setTimeout(() => {
-    request(options, (error, response, body) => {
-      const json = JSON.parse(body);
-      after = json.pagination.cursor;
-      json.data.forEach(el => console.log(`${el.id} ${el.title}`)); // _id no-underscore-dangle
-    });
-  }, 1000);
+const getStreams = (callback) => {
+  request(options, (error, response, body) => {
+    counterTimes += 1;
+    const json = JSON.parse(body);
+    after = json.pagination.cursor;
+    json.data.forEach(el => console.log(`${el.id} ${el.title}`));
+    callback();
+  });
 };
-const getTopStreamsOfLOL = (num) => {
-  for (let i = 0; i < num / 100; i++) topStreamsOfLOL();
+const lastTimeToGetStreams = () => {
+  request(options, (error, response, body) => {
+    const json = JSON.parse(body);
+    after = json.pagination.cursor;
+    json.data.forEach(el => console.log(`${el.id} ${el.title}`));
+  });
+};
+const changeURL = () => {
+  if (counterTimes < requestTimes - 1) {
+    options.url = `https://api.twitch.tv/helix/streams?game_id=21779&first=100&after=${after}`;
+    getStreams(changeURL);
+  } else { // 最後一次不用再呼叫一次 changURL
+    options.url = `https://api.twitch.tv/helix/streams?game_id=21779&first=100&after=${after}`;
+    lastTimeToGetStreams();
+  }
 };
 
-getTopStreamsOfLOL(200);
+changeURL(); // changeURL(requestTimes) 還沒想到
