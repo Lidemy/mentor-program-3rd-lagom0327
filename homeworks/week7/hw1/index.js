77d9ef3480ff05fa30099855aacf39f6da495945
lagom0327 <jz00z0019@gapp.nthu.edu.com>
2019-06-04 05:24:19 +0800

    week7 hw1 add control by key

--- homeworks/week7/hw1/index.js
+++ homeworks/week7/hw1/index.js
@@ -1,10 +1,11 @@
 const bg = document.querySelector('.bg');
 const btn = bg.querySelector('.btn');
+const rank = bg.querySelector('aside');
 let startTime;
 let endTime;
 let ifFall = false;
-let succeed = false;
 let ifColored = false;
+let ifGameStart = false;
 const name = ['金在奐', 'TWICE', 'DreamNote', '尹智聖', 'EXID', 'GFRIEND', 'OH MY GIRL'];
 const colorCollection = [['#48A9C5', '#A0D1CA', '#B4B5DF'], ['#FF5FA2', '#ECCBA6'], ['#00B0A3', '#FFCB05'], ['#5DDBD3', '#DDE4E6', '#EEBAE9'], ['#7474C1', '#E06287', '#F1E6B2'], ['#5F4B8B', '#00ABC0', '#F0EEE9'], ['#F4A6D7', '#9ADBE8', '#EEE29F']];
 // https://www.dcard.tw/f/entertainer/p/229848093
@@ -20,12 +21,22 @@ const selectColor = () => {
   return [`${func[numForFunc][0]}(${func[numForFunc][1][numForType]}, ${colorCollection[numForColor]})`, colorCollection[numForColor][0], colorCollection[numForColor][1], numForColor];
 };
 
-console.log(selectColor('btn-bg'));
-console.log(selectColor('border-bg'));
+const timeCollection = [];
+const addRank = () => {
+  bg.querySelector('.rank').innerText = '';
+  timeCollection.sort((a, b) => a - b);
+  const length = timeCollection.length < 3 ? timeCollection.length : 3;
+  for (let i = 0; i < length; i++) {
+    const li = document.createElement('li');
+    li.innerText = `${timeCollection[i]} s`;
+    bg.querySelector('.rank').appendChild(li);
+  }
+};
 
 const changeColor = (color) => {
   if (!ifFall) {
-    // bg.classList.add('colored');
+    rank.color = `${color[1]}`;
+    rank.style.borderColor = `${color[1]}`;
     bg.style.background = `${color[0]}`;
     startTime = performance.now();
     ifColored = true;
@@ -36,32 +47,52 @@ const changeColor = (color) => {
 function gameStart() {
   const time = Math.floor(Math.random() * 3000);
   const colorResult = selectColor();
-  // const who = document.createTextNode(`這是 ${name[colorResult[3]]} 的應援色`);
+  ifGameStart = true;
   btn.style.background = `${colorResult[1]}`;
   btn.style.borderColor = `${colorResult[2]}`;
   setTimeout(() => changeColor(colorResult), time);
   bg.querySelector('.who').innerText = '';
 }
 
-bg.addEventListener('onload', gameStart());
+const reStart = () => {
+  ifFall = false;
+  ifColored = false;
+  bg.style.background = 'grey';
+  btn.classList.remove('show_btn');
+  gameStart();
+};
 
+const check = () => {
+  if (!ifColored) {
+    alert('還沒變色喔 ~ 失敗!');
+  } else {
+    const time = ((endTime - startTime) / 1000).toFixed(2);
+    alert(`你的成績 : ${time} 秒`);
+    timeCollection.push(time);
+    addRank();
+  }
+  btn.classList.add('show_btn');
+};
+
+document.addEventListener('onload', gameStart());
+
+document.addEventListener('keydown',
+  (e) => {
+    if (e.keyCode === 32) {
+      if (ifGameStart === true) {
+        endTime = performance.now();
+        ifGameStart = false;
+        check();
+      }
+    }
+    if (e.keyCode === 82) reStart();
+  });
 bg.addEventListener('click',
   (e) => {
-    endTime = performance.now();
-    btn.classList.add('show_btn');
-    if (e.target.id === 'btn') {
-      ifFall = false;
-      succeed = false;
-      ifColored = false;
-      bg.style.background = 'grey';
-      btn.classList.remove('show_btn');
-      gameStart();
-    }
-    if (!ifColored && e.target.id !== 'btn') {
-      ifFall = true;
-      alert('還沒變色喔 ~ 失敗!');
-    } else if (!succeed && e.target.id !== 'btn') {
-      alert(`你的成績 : ${((endTime - startTime) / 1000).toFixed(2)} 秒`);
-      succeed = true;
+    if (e.target.id === 'btn') reStart();
+    else {
+      endTime = performance.now();
+      ifGameStart = false;
+      check();
     }
   });
