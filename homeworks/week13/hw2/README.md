adaf808be35fb4b01b79705b150aae3fe584bcd9
lagom0327 <jz00z0019@gapp.nthu.edu.com>
2019-07-15 07:31:58 +0800

    week13 hw2

---
+++ homeworks/week13/hw2/README.md
@@ -0,0 +1,9 @@
+## hw2：超基礎 Todo list
+
+![](todo.png)
+
+請你完成一個很簡單的 Todo List，需要有以下功能：
+
+1. 身為使用者，我可以新增 todo item ，按 `Enter` 或按按紐
+2. 身為使用者，我可以刪除 todo item
+3. 身為使用者，我可以標記某個 todo item 已完成
---
+++ homeworks/week13/hw2/index.js
@@ -0,0 +1,32 @@
+const addTodoItem = () => {
+  if ($('.todo__add_input').val() === '') return null;
+  $('.todo__items').append(`<li class='col-12 todo__item'>
+  <label class=''> ${$('.todo__add_input').val()}
+    <input type='checkbox' >
+    <i class="fas fa-square checkmark"></i>
+    <i class="fas fa-check-square checkmark--checked"></i>
+  </label>
+  <button class="todo__item__delete_btn"><i class="fas fa-times"></i></button>
+</li>`);
+  $('.todo__add_input').val('');
+  return null;
+};
+
+const deleteTodoItem = (target) => {
+  $(target).closest('.todo__item').remove();
+};
+
+
+$(document).ready(() => {
+  $('.todo__add_input ~ button').click(() => {
+    addTodoItem();
+  });
+  $('.todo__add_input').keyup((e) => {
+    if (e.keyCode === 13) addTodoItem();
+  });
+  $('.todo__items').click((e) => {
+    if ($(e.target).closest('.todo__item__delete_btn').length) {
+      deleteTodoItem(e.target);
+    }
+  });
+});
