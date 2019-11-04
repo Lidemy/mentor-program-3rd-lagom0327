d1b1557ee430b0375b6950ab66c25c5f64f1c9d6
lagom0327 <jz00z0019@gapp.nthu.edu.com>
2019-11-04 09:55:56 +0800

    todolist CORS HTTP Header

--- homeworks/week19/hw1/api.php
+++ homeworks/week19/hw1/api.php
@@ -1,7 +1,12 @@
 <?php
   require_once('./conn.php');
   require_once('./isAuthor.php');
-  header('Content-Type: application/json; charset=UTF-8'); 
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PATCH,DELETE");
+  header('Content-Type: application/json; charset=UTF-8');
+  // for preflight
+  header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
+  header('Access-Control-Max-Age: 86400'); // 24hr 指定本次 Preflight Request 的有效期限，以秒為單位
 
   session_start();
 
@@ -11,8 +16,10 @@
 
 // 拿到目前使用者所有的 Todo Items 
   function getAllTodoItems($conn) {
+    // userId = 63 為 guest 用 CORS
+    $userId = !empty($_SESSION['user_id']) ? $_SESSION['user_id'] : 63;
     $stmt = $conn->prepare("SELECT * FROM lagom0327_todoItems WHERE user_id=? AND is_deleted=0");
-    $stmt->bind_param("i", $_SESSION['user_id']);
+    $stmt->bind_param("i", $userId);
     $stmt->execute();
     $result = $stmt->get_result();
     $arr = array();
@@ -49,9 +56,9 @@
 }
 
 // 新增 todo item
-function addTodoItem($conn, $content) {
+function addTodoItem($conn, $userId, $content) {
   $stmt = $conn->prepare("INSERT INTO lagom0327_todoItems(user_id, content) VALUES (?, ?)");
-  $stmt->bind_param("is", $_SESSION['user_id'], $content);
+  $stmt->bind_param("is", $userId, $content);
   if ($stmt->execute()) {
     echo json_encode(array('id' => $conn->insert_id));
   }
@@ -62,7 +69,6 @@ function addTodoItem($conn, $content) {
 // 修改 todo item 的內容
 function editeTodoItem($conn, $id, $content) {
   $stmt = $conn->prepare("UPDATE lagom0327_todoItems SET content=? WHERE id=?");
-  // $stmt->bind_param("si", $_POST['content'], $_POST['id']);
   $stmt->bind_param("si", $content, $id);
   if (!$stmt->execute()) exit(json_encode('fail'));
   echo json_encode('success');
@@ -78,28 +84,54 @@ function toggleTodoItemStatus($conn, $id) {
 
 $method = $_SERVER['REQUEST_METHOD'];
 
+function notAuthor() {
+  echo(json_encode('You aren\'t the author of this Todo Item!'));
+  header('HTTP/1.1 401  Unauthorized');
+}
+
+function noParameter($str) {
+  echo(json_encode('lack of ' . $str));
+  header('HTTP/1.1 400 bad request');
+}
 switch ($method) {
+
+  
   case 'GET':
     if (isset($_GET['id']) && !empty($_GET['id'])) {
-      if (!isItemAuthor($conn)) exit(json_encode('You don\'t have permission to get this Todo Item!'));
+      if (!isItemAuthor($conn)) exit(notAuthor());
       getTodoItemById($conn, $_GET['id']);
     } 
     else getAllTodoItems($conn);
     break;
 
   case 'POST':
-      if(isset($_POST['id']) && !isset($_POST['content']) && isItemAuthor($conn)) exit(toggleTodoItemStatus($conn, $_POST['id']));
-      if (empty($_POST['content'])) die('empty data');
-      if (isset($_POST['userId']) && !isset($_POST['id']) && (int)$_POST['userId'] === $_SESSION['user_id']) exit(addTodoItem($conn, $_POST['content']));
-      if (isset($_POST['id']) && isItemAuthor($conn)) { // TODO
-        exit(editeTodoItem($conn, $_POST['id'], $_POST['content']));
-      }
+      if (empty($_POST['content'])) exit(noParameter('content'));
+      if (!isset($_POST['id']) && empty($_SESSION['user_id'])) exit(addTodoItem($conn, 63, $_POST['content']));
+      if (isset($_POST['userId']) && !isset($_POST['id']) && (int)$_POST['userId'] === $_SESSION['user_id']) exit(addTodoItem($conn, $_SESSION['user_id'], $_POST['content']));
     break;
+
+  case 'PATCH':
+    $str = file_get_contents("php://input");
+    $data = array();
+    parse_str($str, $data);
+    if(!isItemAuthorForPatch($conn, $data['id'])) exit(notAuthor());
+    if (isset($data['id']) && !isset($data['content'])) exit(toggleTodoItemStatus($conn, $data['id']));
+    if (empty($data['content']) || empty($data['id'])) exit(noParameter('parameter'));
+    exit(editeTodoItem($conn, $data['id'], $data['content']));
+    break;
+
   case 'DELETE':
-    if (!isset($_GET['id']) || empty($_GET['id'])) exit('id');
-    if (!isItemAuthor($conn)) exit('author');
+    if (!isset($_GET['id']) || empty($_GET['id'])) exit(noParameter('id'));
+    if (!isItemAuthor($conn)) exit(notAuthor());
     deleteTodoItem($conn);
     break;
+  case 'OPTIONS':
+    header("HTTP/1.1 204 No Content");
+    break;
+  default:
+    header('HTTP/1.1 404 Not Found');
+    break;
+
 }
 
 ?>
--- homeworks/week19/hw2/isAuthor.php
+++ homeworks/week19/hw2/isAuthor.php
@@ -7,6 +7,7 @@ function check($conn, $id) {
   $stmt->execute();
   $result = $stmt->get_result();
   if ($result) {
+    if ($result->num_rows <= 0) exit(header('HTTP/1.1 400 error: bad request'))
     $row = $result->fetch_assoc();
     // id = 63 is guest
     if(empty($_SESSION['user_id'])) return ((int)$row['user_id'] === 63);
